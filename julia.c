666870c89967bb16002ff2c5bc244d85f50602e0
John Champagne <johnchampagne97@gmail.com>
2020-05-13 12:43:36 -0500

    added more dynamic processing (rather than preset globals)

--- julia.c
+++ julia.c
@@ -3,10 +3,10 @@
 #include <complex.h>
 #include <time.h>
 
-#define __SIZEX 3000
+#define __SIZEX (1920*10)
 const int size_x = __SIZEX;
 
-#define __SIZEY 3000
+#define __SIZEY (1920*10)
 const int size_y = __SIZEY;
 
 float M[__SIZEX][__SIZEY];
@@ -21,8 +21,8 @@ float M[__SIZEX][__SIZEY];
 #define __IM_UPPER (__Y + __DY)
 #define __IM_LOWER (__Y - __DY)
 
-#define __RE_MAP(x) (__RE_LOWER + x*(__RE_UPPER - __RE_LOWER)/__SIZEX)
-#define __IM_MAP(x) (__IM_LOWER + x*(__IM_UPPER - __IM_LOWER)/__SIZEY)
+#define __RE_MAP(x,l,u) ((l) + (x)*((u) - (l))/__SIZEX)
+#define __IM_MAP(x,l,u) ((l) + (x)*((u) - (l))/__SIZEY)
 
 #define __POWER 2
 #define __TH (M_PI * 0.5)
@@ -70,42 +70,60 @@ char* HSLtoRGB(float H, float S, float L, char*r){
     return r;
 }
 
+typedef struct julia {
+    double x,y,dx,dy;
+    double complex power;
+    double complex c;
 
-int main(){
+
+    double R;
+    int max_n;
+
+    int min,max;
+
+    char* filename;
+} julia_t;
+
+int write_image(julia_t* Set){
+    Set->min = Set->max_n;
+    Set->max = 0;
     clock_t t = clock();
     for (int x = 0; x < size_x; x++)
         for (int y = 0; y < size_y; y++) {
-            double complex z = __RE_MAP((double)x) + __IM_MAP((double)y)*I;
-            M[x][y] = max_n;
-            for (int i = 0; i < max_n; i++){
-                if (cabs(z) > __R) {
+            double complex z = __RE_MAP((double)x,Set->x - Set->dx, Set->x + Set->dx) + __IM_MAP((double)y,Set->y - Set->dy, Set->y + Set->dy)*I;
+            M[x][y] = Set->max_n;
+            for (int i = 0; i < Set->max_n; i++){
+                if (cabs(z) > Set->R) {
                     M[x][y] = i;
-                    //printf("%d\n", i);
+                    if (i > Set->max)
+                        Set->max = i;
+                    if (i < Set->min)
+                        Set->min = i;
                     break;
                 }
-                z = cpow(z,__POWER) + __CONST;
+                z = cpow(z,Set->power) + Set->c;
             }
         }
     t = clock() - t;
-    //printf("Computation time: %f\n", (double)t / CLOCKS_PER_SEC);
-    FILE *fp = fopen("julia.ppm", "wb");
+    printf("%f, Max %d, Min %d\n", (double)t / CLOCKS_PER_SEC, Set->max, Set->min);
+    FILE *fp = fopen(Set->filename, "wb");
     fprintf(fp, "P6\n%d %d\n255\n", size_x, size_y);
     for (int x = 0; x < size_x; x++)
         for (int y = 0; y < size_y; y++){
             unsigned char C[3] = {0xff, 0xff, 0xff};
             // Convergent Point
-            if (M[x][y] == max_n){
+            if (M[x][y] == Set->max_n){
                 C[0] = 0;
                 C[1] = 0;
                 C[2] = 0;
             }
             else {
-                double S = (double)M[x][y] / max_n;
+                double S = fmod(((double)M[x][y] - Set->min)/(Set->max - Set->min), 1.0);
                 if (S > 1.0)
                     S = 1.0;
                 else if (S < 0)
                     S = 0.0;
-                HSLtoRGB(S,1,0.5,C);
+                HSLtoRGB(99.0/360.0,1,S,C);
             }
             fwrite(C, 1, 3, fp);
         }
@@ -114,7 +132,25 @@ int main(){
 }
 
 
-
+int main(){
+    julia_t J;
+    J.x = 0;
+    J.y = 0;
+    J.dx = 1.5;
+    J.dy = 1.5;
+    J.power = 2;
+    J.c = 0.5 * cexp(I*M_PI*0.25);
+    J.max_n = 1000;
+    J.R = 1E300;
+    for (int i = 0; i < 1; i++){
+        //J.c = 0.5 * cexp(I * (double)(i) / 360.0 * 2.0 * M_PI);
+        char filename[50];
+        //sprintf(filename, "%03d.ppm", i);
+        sprintf(filename, "julia.ppm");
+        J.filename = filename;
+        write_image(&J);
+    }
+}
 
 
 
